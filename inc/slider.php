5a645d09dcee9953eec35aa1a5e9431afd3c7838
Trần Tuấn Anh <tuananhniichan@gmail.com>
2022-11-24 17:59:01 +0700

    Create slider
    Created slider

---
+++ inc/slider.php
@@ -0,0 +1,21 @@
+<div id="carouselSlider" class="carousel slide" data-ride="carousel" style="overflow:hidden">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/slider-banner-001.jpg" class="d-block w-100" alt="banner-001">
+    </div>
+    <div class="carousel-item">
+      <img src="images/slider-banner-002.jpg" class="d-block w-100" alt="banner-002">
+    </div>
+    <div class="carousel-item">
+      <img src="images/slider-banner-003.jpg" class="d-block w-100" alt="banner-003">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselSlider" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselSlider" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
